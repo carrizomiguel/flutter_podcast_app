7e6fe9595d881fd74c63a095b463d5e0c198bf4a
Miguel Carrizo <carrizo2000miguel@gmail.com>
2023-01-21 22:43:49 -0500

    fix: :pencil2: deprecated method deleted

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -17,13 +17,13 @@ import 'package:podcast_app/ui/home/data/repositories/home_repository.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
-  void onChange(BlocBase bloc, Change change) {
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
     log('onChange(${bloc.runtimeType}, $change)');
   }
 
   @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     log('onError(${bloc.runtimeType}, $error, $stackTrace)');
     super.onError(bloc, error, stackTrace);
   }
@@ -33,6 +33,8 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
+
+  Bloc.observer = AppBlocObserver();
 
   await runZonedGuarded(
     () async {
@@ -63,36 +65,33 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
       );
       final detailRemoteDataSource = DetailRemoteDataSourceImpl();
 
-      await BlocOverrides.runZoned(
-        () async => runApp(
-          MultiRepositoryProvider(
-            providers: [
-              RepositoryProvider<HomeRepositoryImpl>(
-                create: (_) => HomeRepositoryImpl(
-                  localDataSource: homeLocalDataSource,
-                  remoteDataSource: homeRemoteDataSource,
-                  networkInfo: networkInfo,
-                ),
+      runApp(
+        MultiRepositoryProvider(
+          providers: [
+            RepositoryProvider<HomeRepositoryImpl>(
+              create: (_) => HomeRepositoryImpl(
+                localDataSource: homeLocalDataSource,
+                remoteDataSource: homeRemoteDataSource,
+                networkInfo: networkInfo,
               ),
-              RepositoryProvider<ExploreRepositoryImpl>(
-                create: (_) => ExploreRepositoryImpl(
-                  localDataSource: exploreLocalDataSource,
-                  remoteDataSource: exploreRemoteDataSource,
-                  networkInfo: networkInfo,
-                ),
+            ),
+            RepositoryProvider<ExploreRepositoryImpl>(
+              create: (_) => ExploreRepositoryImpl(
+                localDataSource: exploreLocalDataSource,
+                remoteDataSource: exploreRemoteDataSource,
+                networkInfo: networkInfo,
               ),
-              RepositoryProvider<DetailRepositoryImpl>(
-                create: (_) => DetailRepositoryImpl(
-                  localDataSource: detailLocalDataSource,
-                  remoteDataSource: detailRemoteDataSource,
-                  networkInfo: networkInfo,
-                ),
+            ),
+            RepositoryProvider<DetailRepositoryImpl>(
+              create: (_) => DetailRepositoryImpl(
+                localDataSource: detailLocalDataSource,
+                remoteDataSource: detailRemoteDataSource,
+                networkInfo: networkInfo,
               ),
-            ],
-            child: await builder(),
-          ),
+            ),
+          ],
+          child: await builder(),
         ),
-        blocObserver: AppBlocObserver(),
       );
     },
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
